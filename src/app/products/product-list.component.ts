5f9b64f61c0f1f29cbcb331060f6725e2a9b4a3b
oviemudi <oviisaacs@gmail.com>
2020-07-23 22:33:25 +0100

    add product interface

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
   selector: 'pm-products',
@@ -10,7 +11,7 @@ export class ProductListComponent {
   imageMargin = 2;
   showImage = false;
   filterList = 'Rake';
-  products: any[] = [
+  products: IProduct[] = [
     {
       productId: 1,
       productName: 'Leaf Rake',
